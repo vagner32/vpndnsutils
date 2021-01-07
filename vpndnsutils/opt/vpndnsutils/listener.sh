42566df467b26b26792699ef96c8b1c1163a1847
Vagner Matos <vagner32@gmail.com>
2021-01-07 13:14:31 -0300

    including fix case the service be removed while the VPN is connected - need fix the resolv.conf link to original

--- vpndnsutils/opt/vpndnsutils/listener.sh
+++ vpndnsutils/opt/vpndnsutils/listener.sh
@@ -1,10 +1,6 @@
 VPN_FILE=/etc/resolv.conf.vpn
 ORIGINAL_FILE=/run/resolvconf/resolv.conf
 SLINK=/etc/resolv.conf
-#if [[ ! -f "$VPN_FILE" ]]; then
-#	echo "nameserver 8.8.8.8" > $VPN_FILE
-#	cat $ORIGINAL_FILE >> $VPN_FILE
-#fi
 
 inotifywait -m /tmp/pritunl/ -e create -e delete |
     while read dir action file; do
@@ -12,15 +8,16 @@ inotifywait -m /tmp/pritunl/ -e create -e delete |
 	echo nameserver  `grep prd /etc/vpndnsutils.conf |awk '{print $3}'` >> $VPN_FILE
 	echo nameserver `grep qa /etc/vpndnsutils.conf |awk  '{print $3}'` >> $VPN_FILE
 	cat $ORIGINAL_FILE >> $VPN_FILE
+	chmod 777 $VPN_FILE
 	if [[ "$file" =~ .*auth$ ]]; then
         echo "The file '$file' appeared in directory '$dir' via '$action'"
-	CURRENT_LINK_TARGET=`readlink -f /etc/resolv.conf`
-	if [[ "$CURRENT_LINK_TARGET" == "$ORIGINAL_FILE" ]]; then #IDA
-	  rm $SLINK
-	  ln -s $VPN_FILE $SLINK
-	else #VOLTA
-	  rm $SLINK
-	  ln -s $ORIGINAL_FILE $SLINK
-	fi
+	    CURRENT_LINK_TARGET=`readlink -f /etc/resolv.conf`
+	    if [[ "$CURRENT_LINK_TARGET" == "$ORIGINAL_FILE" ]]; then #IDA
+	      	rm $SLINK
+	      	ln -s $VPN_FILE $SLINK
+	    else #VOLTA
+	      	rm $SLINK
+	      	ln -s $ORIGINAL_FILE $SLINK
+		fi
 	fi
     done
